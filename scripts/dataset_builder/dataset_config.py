2552084d14fc47df5b81be566365f121eee5cbe5
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2022-03-21 13:55:10 +0100

    Add config for dataset_builder.py

---
+++ scripts/dataset_builder/dataset_config.py
@@ -0,0 +1,5 @@
+image_path = "/commons/Themas/Thema11/Giepmans/Maps_satelliteBW.tif"
+classifier_path = "/commons/Themas/Thema11/Giepmans/Maps_labels.tif"
+save_location = "/homes/kanotebomer/Documents/Thema11/Project-Ra/scripts/dataset_builder/"
+gaussian_layers = 1
+window = (3, 3)
