ae527a25af6aa3390afa266e541fc68704d694bd
sjbouwman <sjbouwman@st.hanze.nl>
2022-04-05 15:10:06 +0200

    Contrast matching script works. But is still hardcoded

--- scripts/image_filters/image_filters/contrast_matching.py
+++ scripts/image_filters/image_filters/contrast_matching.py
@@ -1,9 +1,27 @@
-import matplotlib
+# import the necessary packages
+from skimage import exposure
+import matplotlib.pyplot as plt
+import argparse
+import cv2
 
-def main():
-    pass
+
+def process(source_path, reference_path):
+    # load the source and reference images
+    print("[INFO] Loading images")
+    source = cv2.imread(source_path)
+    reference = cv2.imread(reference_path)
+
+    # determine if we are performing multichannel histogram matching
+    # and then perform histogram matching itself
+    print("[INFO] Performing matching")
+    multi = True if source.shape[-1] > 1 else False
+    matched = exposure.match_histograms(source, reference, channel_axis=multi)
+    # show the output images
+    cv2.imwrite('Tile_r4-c4_Acquisition Spec 3_452994970_matched.tif', matched)
+    print("Done")
 
 
 if __name__ == '__main__':
-    main_image = ""
-    matcher_image = ""
+    source_image = "data/Tile_r4-c4_Acquisition Spec 3_452994970.tif"
+    reference_image = "data/Tile_r4-c7_Acquisition_Spec_3_452994970.tif"
+    process(source_image, reference_image)
