a30d1450f03bf4cad71c424353e0c6a1581914e9
Reindert1 <r.f.visser@st.hanze.nl>
2022-04-06 15:39:22 +0200

    debugging booster model export

--- scripts/machine_learning/adabooster.py
+++ scripts/machine_learning/adabooster.py
@@ -15,7 +15,7 @@ __version__ = 0.1
 # Imports
 import pickle
 import sys
-
+import booster2 as model2
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -36,23 +36,15 @@ class AdaBoost:
         AdaBoost.export_model(self)
 
     def fit(self):
-        x_train, x_val, y_train, y_val = train_test_split(self.data_array[:, :-1], self.data_array[:, -1], random_state=0)
+        x_train, x_val, y_train, y_val = train_test_split(self.data_array[:, :-1],
+                                                          self.data_array[:, -1],
+                                                          random_state=42)
         self.x_val = x_val
         print("Initializing booster...")
-        model = AdaBoostClassifier(n_estimators=5, learning_rate=0.1, algorithm='SAMME')
+        model = AdaBoostClassifier(n_estimators=10, learning_rate=0.1, algorithm='SAMME')
 
         print("Fitting training data...")
         model.fit(x_train, y_train)
-
-        prediction = model.score(x_train, y_train)
-
-        print('The accuracy on training data is: ', prediction * 100, '%')
-
-        print("Running validation data...")
-        model.predict(x_val)
-
-        prediction = model.score(x_val, y_val)
-        print('The accuracy on validation data is: ', prediction * 100, '%')
 
         # Setting model
         self.model = model
@@ -64,18 +56,26 @@ class AdaBoost:
         AdaBoost.model_to_tif(self)
 
     def model_to_tif(self):
-        palettedata = [0, 0, 0, 0, 0, 255]
+        data_array = np.load("r4-c7_nucleus.npy", allow_pickle=True)[:, :-1]
+        palettedata = []
         num_entries_palette = 256
+        for _ in range(1):
+            palettedata.extend(list(np.random.choice(range(256), size=3)))
+            print(palettedata)
         num_bands = len("RGB")
         num_entries_data = len(palettedata) // num_bands
-        palettedata.extend(palettedata[:num_bands]
+        palettedata.extend([0, 0, 0]
                            * (num_entries_palette
                               - num_entries_data))
+        save_image("Booster.sav",
+                     data_array, palettedata)
 
-        loaded_model = self.model
-        full_pred = loaded_model.predict(self.x_val).reshape(16384, 16384)
+    def save_image(self, data, palette):
+        loaded_model = pickle.load(model2)
+        full_pred = loaded_model.predict(data).reshape(16384, 16384)
+        print(np.unique(full_pred))
         full_image = Image.fromarray(full_pred, mode="P")
-        full_image.putpalette(palettedata)
+        full_image.putpalette(palette)
         full_image.save("Booster.tif")
 
 
