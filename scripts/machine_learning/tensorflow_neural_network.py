3d6c60995050bac8f2ab9df68d2476a311a2fbbd
Reindert1 <r.f.visser@st.hanze.nl>
2022-05-17 09:35:53 +0200

    working model

--- scripts/machine_learning/tensorflow_neural_network.py
+++ scripts/machine_learning/tensorflow_neural_network.py
@@ -2,49 +2,49 @@ import numpy as np
 import tensorflow as tf
 import pickle
 from sklearn.model_selection import train_test_split
+from tensorflow import estimator
+
 print("TensorFlow version:", tf.__version__)
 
-BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 100
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(None, 64, 14)),
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(109,)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(64, activation='sigmoid'),
+    tf.keras.layers.Dense(32)
 ])
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+model.compile(optimizer=tf.keras.optimizers.Adam(),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['sparse_categorical_accuracy'])
+              metrics=['accuracy'])
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 
 def main():
-    data = np.load("/Users/rfvis/Documents/GitHub/Project-Ra/data/r4-c7_nucleus.npy", allow_pickle=True)
+    data = np.load("/Users/rfvis/Documents/GitHub/Project-Ra/data/dataset/full_classification.npy", allow_pickle=True)
+    print(np.shape(data))
+    x_train, x_test, y_train, y_test = train_test_split(data[:, :-1],
+                                                        data[:, -1],
+                                                        random_state=42)
 
-    x_train, x_val, y_train, y_val = train_test_split(data[:, :-1],
-                                                      data[:, -1],
-                                                      random_state=42)
     print("Fit model on training data")
     history = model.fit(
         x_train,
         y_train,
-        batch_size=64,
-        epochs=2,
-        validation_data=(x_val, y_val),
+        epochs=10,
+        validation_data=(x_test, y_test),
     )
+
+    print("Using model:\n", model)
 
     # Evaluate the model on the test data using `evaluate`
     print("Evaluate on test data")
-    results = model.evaluate(x_val, y_val, batch_size=128)
+    results = model.evaluate(x_test, y_test, batch_size=150)
     print("test loss, test acc:", results)
 
-    # Generate predictions (probabilities -- the output of the last layer)
-    # on new data using `predict`
-    print("Generate predictions for 3 samples")
-    predictions = model.predict(x_val[:50])
-    print("predictions shape:", predictions.shape)
+    predictions = model.predict(x_test)
 
     return 0
 
