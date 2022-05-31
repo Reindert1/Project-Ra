b974733e140760e5a9d1c113983c611fe58f3e74
devalk96 <sjbouwman@outlook.com>
2022-05-31 15:48:38 +0200

    Updated tensor to tiff, now correctly creating images

--- scripts/machine_learning/tensor_to_tiff.py
+++ scripts/machine_learning/tensor_to_tiff.py
@@ -1,26 +1,19 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 """
 Script to segment a given image using a given classifier
 """
 
-__author__ = "Skippybal & Reindert1"
-__version__ = "0.1"
+__author__ = "Skippybal, Reindert & Sander"
+__version__ = "0.2"
 
-import math
-import pickle
-import random
 
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
-import h5py
 import cv2 as cv
-import gc
-from sklearn.model_selection import train_test_split
-from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Sequential, save_model, load_model
 
 
 def validation_generator(instances, batch_size):
@@ -29,24 +22,22 @@ def validation_generator(instances, batch_size):
 
 
 def model_to_tif(model_file, save_loc, dataset_loc, palette, original_image_loc):
-
     loaded_model = keras.models.load_model(model_file)
     loaded_model.summary()
     x_data = np.load(dataset_loc, mmap_mode="r")
     x_data = x_data[:, :-1]
-    full_pred = []
 
-    for val_batch in validation_generator(x_data, 500):
-        pred = loaded_model(np.array(val_batch))
-        full_pred.extend(tf.argmax(pred, 1))
+    predictions = loaded_model.predict(x_data)
 
-    full_pred = np.asarray(full_pred)
     image_array = cv.imread(original_image_loc, cv.IMREAD_GRAYSCALE)
-    shape = image_array.shape
-    full_pred = np.reshape(full_pred, shape)
-    full_image = Image.fromarray(full_pred, mode="P")
-    full_image.putpalette(palette)
-    full_image.save(save_loc)
+    shape = image_array.shape + (1,)
+
+    output = tf.argmax(predictions, axis=1)
+    output = tf.reshape(output, shape)
+
+    plt.figure(figsize=(15, 15))
+    plt.imsave(save_loc, tf.keras.utils.array_to_img(output))
+    plt.imshow(tf.keras.utils.array_to_img(output))
 
 
 def main():
@@ -63,10 +54,10 @@ def main():
                        * (num_entries_palette
                           - num_entries_data))
 
-    model_location = ...
-    save_location = ...
-    data_location = ...
-    original_location = ...
+    model_location = "model/saved_model/new_model.h5"
+    save_location = "model/saved_model/img/tensorflow_modelv2.png"
+    data_location = "model/full_classification.npy"
+    original_location = "model/nucleus_color_cleaned.tif"
 
     model_to_tif(model_location, save_location, data_location, palettedata, original_location)
 
