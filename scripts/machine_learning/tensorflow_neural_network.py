0272ad8d6b3733aa331c3787890b32438d8c2e0d
Reindert1 <r.f.visser@st.hanze.nl>
2022-05-31 15:33:00 +0200

    updated model with plotting and early stopping

--- scripts/machine_learning/tensorflow_neural_network.py
+++ scripts/machine_learning/tensorflow_neural_network.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import pickle
@@ -12,8 +13,10 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(128, activation='relu', input_shape=(109,)),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(128, activation='tanh'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(32, activation='tanh'),
+    tf.keras.layers.Dense(64, activation='swish'),
+    tf.keras.layers.Dense(32, activation='swish'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(16, activation='tanh'),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
 
@@ -24,24 +27,35 @@ model.compile(optimizer=tf.keras.optimizers.Adam(),
 
 def main():
     data = np.load("/Users/rfvis/Documents/GitHub/Project-Ra/data/dataset/full_classification.npy", allow_pickle=True)
-    print(np.shape(data))
-    x_train, x_test, y_train, y_test = train_test_split(data[:, :-1],
-                                                        data[:, -1],
-                                                        random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(data[:, :-1], data[:, -1], random_state=69)
+    callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=4)
     print("Fit model on training data")
-    history = model.fit(
-        x_train,
-        y_train,
-        epochs=5,
-    )
-    # Evaluate the model on the test data using `evaluate`
+    history = model.fit(x_train, y_train, epochs=150, validation_split=0.25, callbacks=[callback])
+
     print("Evaluate on test data")
     results = model.evaluate(x_test, y_test, batch_size=150)
     print("test loss, test acc:", results)
 
     print("saving model!")
-    model.save('saved_model/my_model.pb')
+    model.save('/Users/rfvis/Documents/GitHub/Project-Ra/data/saved_model/new_model.h5')
 
+    print("Plotting results")
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    print("Plotting loss")
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
     return 0
 
 
